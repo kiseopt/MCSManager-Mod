bd3cfe60053011cac396acc0984e45d9621a6509
Kiseopt <hibiya@live.cn>
2019-10-29 01:17:56 +0800

    修正 EULA.txt 写入时 fs.writeFile 的兼容性问题，使其兼容 node v10 以上版本

--- model/ServerModel.js
+++ model/ServerModel.js
@@ -22,7 +22,7 @@ module.exports.createServerDir = (serverName, cwd) => {
         fs.mkdirSync(cwd);
     }
     //EULA.txt 写入
-    fs.writeFile(cwd + '/eula.txt', 'eula=true');
+    fs.writeFile(cwd + 'eula.txt', 'eula=true','utf8',() => {});
 }
 
 module.exports.createServer = (serverName, config) => {
